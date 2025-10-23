d69665c8a8c356829583f32a6ee711cb6f18c09b
anas-abdi <anasabdulaziiz21@gmail.com>
2025-10-23 10:05:22 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Greetings from './assets/component/Greetings'
+import Header from './assets/component/Header'
+function App() {
+  
+
+  return (
+    <>
+      {/* <Greetings name="Anas"/>
+      <Header />    */}
+      <Header /> 
+      <Header /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/component/Greetings.jsx
@@ -0,0 +1,8 @@
+// in App.jsx
+function Greetings(props) {
+    return <h1>Hello, {props.name}!</h1>;
+}
+  
+
+
+export default Greetings;
